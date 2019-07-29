ea19aafcc9529ffc2b0ab2faca3810dbd2b2c2c3
Max <mcferrenmax@gmail.com>
2019-07-29 17:46:20 -0400

    setup basic register logic

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,16 +1,18 @@
 from flask import request
 from flask_login import login_user, logout_user, login_required
 from . import auth
+from .. import db
 from ..models import User
 
 
-@auth.route('/login', methods=['GET', 'POST'])
+@auth.route('/login', methods=['POST'])
 def login():
-    req = request.get_json()
-    user = User.query.filter_by(email=req['email']).first()
-    if user is not None and user.verify_password(req['password']):
-        login_user(user, req['remember_me'])  # remember me True
-        return '<h1>Hello {}</h1>'.format(user.username)
+    json_req = request.get_json()
+    user = User.query.filter_by(email=json_req['email']).first()
+    if user is not None and user.verify_password(json_req['password']):
+        login_user(user, json_req['remember_me'])  # remember me True
+        # TODO(max): return json response
+        return '<h1>Welcome back {}</h1>'.format(user.username)
     else:
         # TODO(max): set status code 404
         return '<h1>User not found</h1>'
@@ -20,3 +22,16 @@ def login():
 @login_required
 def logout():
     logout_user()
+
+
+@auth.route('/register', methods=['POST'])
+def register():
+    json_req = request.get_json()
+    user = User(email=json_req['email'],
+                username=json_req['username'],
+                password=json_req['password'])
+    # TODO(max): What if user already exists?
+    db.session.add(user)
+    db.session.commit()
+    # TODO(max): return json response
+    return '<h1>{} is now registered</h1>'.format(user.username)
