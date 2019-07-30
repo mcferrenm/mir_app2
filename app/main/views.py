a2e0a4f0df1a80f7865ffcefb7831244839f3e8c
Max <mcferrenmax@gmail.com>
2019-07-29 20:25:33 -0400

    build edit-profile route

--- app/main/views.py
+++ app/main/views.py
@@ -1,18 +1,24 @@
-from flask import session
+from flask import session, request
+from flask_login import login_required, current_user
 from . import main
 from .. import db
 from ..models import User
 
 
-@main.route('/<username>', methods=['GET', 'POST'])
-def index(username):
-    user = User.query.filter_by(username=username).first()
-    if user is None:
-        user = User(username=username)
-        db.session.add(user)
-        db.session.commit()
-        session['known'] = False
-    else:
-        session['known'] = True
-    session['name'] = username
-    return '<h1>Ayyy !{}</h1>'.format(session.get('name'))
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return '<h1>Ayyy !{}</h1>'.format(user.username)
+
+
+@main.route('/edit-profile', methods=['POST'])
+@login_required
+def edit_profile():
+    json_req = request.get_json()
+    current_user.name = json_req['name']
+    current_user.location = json_req['location']
+    current_user.about_me = json_req['about_me']
+    db.session.add(current_user._get_current_object())
+    db.session.commit()
+    # TODO(max): proper JSON responses and error handling / validation
+    return '<h1>Updated {}</h1>'.format(current_user.name)
